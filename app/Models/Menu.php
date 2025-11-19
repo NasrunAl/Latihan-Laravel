550019b8107ec7d31daec41131d3d4bd4b277235
Nasrun Al <e42241414@student.polije.ac.id>
2025-11-19 21:42:24 +0700

    database crud

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -11,6 +11,7 @@ class Menu extends Model
 
     protected $fillable = [
         'nama_menu',
+        'deskripsi',
         'foto',
         'harga'
     ];
--- database/migrations/2025_11_19_143042_create_menus_table.php
+++ database/migrations/2025_11_19_143042_create_menus_table.php
@@ -4,20 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-    public function up()
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('menus', function (Blueprint $table) {
             $table->id();
-            $table->string('nama_menu');
-            $table->text('deskripsi');
-            $table->integer('harga');
-            $table->string('foto')->nullable();
             $table->timestamps();
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('menus');
     }
